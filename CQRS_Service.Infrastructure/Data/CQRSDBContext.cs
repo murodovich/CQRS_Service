8fa6c784566c3abb8770691c942684388f8977e4
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-17 18:29:09 +0500

    dadsa

--- CQRS_Service.Infrastructure/Data/CQRSDBContext.cs
+++ CQRS_Service.Infrastructure/Data/CQRSDBContext.cs
@@ -1,6 +1,15 @@
-﻿namespace CQRS_Service.Infrastructure.Data
+﻿using CQRS_Service.Application.Absreactions;
+using CQRS_Service.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS_Service.Infrastructure.Data
 {
-    public class CQRSDBContext
+    public class CQRSDBContext : DbContext,ICQRSDBContext
     {
+        public CQRSDBContext(DbContextOptions<CQRSDBContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
     }
 }
