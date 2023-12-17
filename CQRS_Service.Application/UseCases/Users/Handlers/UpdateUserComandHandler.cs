c39c8535bbb922b4c64005a05e63323d75ae2424
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-17 18:28:45 +0500

    fsvsdsd

---
+++ CQRS_Service.Application/UseCases/Users/Handlers/UpdateUserComandHandler.cs
@@ -0,0 +1,35 @@
+﻿using CQRS_Service.Application.Absreactions;
+using CQRS_Service.Application.UseCases.User.Commands;
+using CQRS_Service.Domain.Exceptions.Users;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS_Service.Application.UseCases.Users.Handlers
+{
+    public class UpdateUserComandHandler : IRequestHandler<UpdateUserCommand, bool>
+    {
+        private readonly ICQRSDBContext _dbContext;
+
+        public UpdateUserComandHandler(ICQRSDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (result == null) throw new UserNotFoundException();
+
+            result.FirstName = request.FirstName;
+            result.LastName = request.LastName;
+            result.Email = request.Email;
+            result.Password = request.Password;
+
+            _dbContext.Users.Update(result);
+
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
