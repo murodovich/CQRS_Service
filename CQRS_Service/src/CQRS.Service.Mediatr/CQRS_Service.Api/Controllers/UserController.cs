1b4c156c5bdbf5233ed08ce4bc25a4161bb7fb8b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-17 17:30:59 +0500

    save

---
+++ CQRS_Service/src/CQRS.Service.Mediatr/CQRS_Service.Api/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using CQRS_Service.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CQRS_Service.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> CreateAsync(User user)
+        {
+            var result = new User()
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Password = user.Password,
+            };
+            await _mediator.Send(user);
+            return Ok("Created");
+        }
+    }
+}
