87510b94f83d6834d0ff030846d46d6b92ce5601
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-17 17:32:09 +0500

    UserCommand

---
+++ CQRS_Service.Application/UseCases/Users/Commands/CreateUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace CQRS_Service.Application.UseCases.User.Commands
+{
+    public class CreateUserCommand : IRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
+ 
