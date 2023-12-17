dc00f4dab2262e9f5596a897b269619dc225eb7f
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-17 18:29:02 +0500

    asdads

---
+++ CQRS_Service.Domain/Exceptions/Users/UserNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace CQRS_Service.Domain.Exceptions.Users
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException(): base("User not found!")
+        {
+            
+        }
+    }
+}
