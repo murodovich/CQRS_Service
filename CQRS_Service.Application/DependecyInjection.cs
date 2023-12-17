cb996848cc68986a7bf5b1dcdbe2018c2c993eb7
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-17 17:32:46 +0500

    dependecyg

---
+++ CQRS_Service.Application/DependecyInjection.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CQRS_Service.Application
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+
+    }
+}
