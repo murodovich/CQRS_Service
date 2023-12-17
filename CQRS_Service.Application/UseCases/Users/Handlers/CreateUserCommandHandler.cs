487fdd36ff0c8f92704af6d56838e2b71c2b4a89
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-17 17:32:29 +0500

    afbscdw

---
+++ CQRS_Service.Application/UseCases/Users/Handlers/CreateUserCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using CQRS_Service.Application.Absreactions;
+using CQRS_Service.Application.UseCases.User.Commands;
+using MediatR;
+
+namespace CQRS_Service.Application.UseCases.Users.Handlers
+{
+    public class CreateUserCommandHandler : AsyncRequestHandler<CreateUserCommand>
+    {
+        private readonly ICQRSDBContext _dbContext;
+
+        public CreateUserCommandHandler(ICQRSDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        protected async override Task Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            var person = new Domain.Entities.User()
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Password = request.Password
+            };
+
+            await _dbContext.Users.AddAsync(person);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
