22a9ca9acd0b767b0ee2c0c80b05972e2c4484ce
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-17 18:28:40 +0500

    fdcsdsv

---
+++ CQRS_Service.Application/UseCases/Users/Handlers/GetByIdUserQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using CQRS_Service.Application.Absreactions;
+using CQRS_Service.Application.UseCases.Users.Queries;
+using CQRS_Service.Domain.Exceptions.Users;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS_Service.Application.UseCases.Users.Handlers
+{
+    public class GetByIdUserQueryHandler : IRequestHandler<GetByIdUserQuery, Domain.Entities.User>
+    {
+        private readonly ICQRSDBContext _dbContext;
+
+        public GetByIdUserQueryHandler(ICQRSDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Domain.Entities.User> Handle(GetByIdUserQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new UserNotFoundException();
+            return result;
+        }
+    }
+}
