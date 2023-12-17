7e9893c130d51713580cf3dbeda760cbef86f47b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-17 18:27:52 +0500

    Program

--- CQRS_Service/src/CQRS.Service.Mediatr/CQRS_Service.Api/Program.cs
+++ CQRS_Service/src/CQRS.Service.Mediatr/CQRS_Service.Api/Program.cs
@@ -1,9 +1,15 @@
+using CQRS_Service.Application;
+using CQRS_Service.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
