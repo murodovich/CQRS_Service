c83b195bdaba1ce78e3540cfc9fede5f10a180a8
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-17 18:28:22 +0500

    fdasdadsa

--- CQRS_Service.Application/UseCases/Users/Commands/UpdateUserCommand.cs
+++ CQRS_Service.Application/UseCases/Users/Commands/UpdateUserCommand.cs
@@ -1,6 +1,13 @@
-﻿namespace CQRS_Service.Application.UseCases.User.Commands
+﻿using MediatR;
+
+namespace CQRS_Service.Application.UseCases.User.Commands
 {
-    public class UpdateUserCommand
+    public class UpdateUserCommand : IRequest<bool>
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
