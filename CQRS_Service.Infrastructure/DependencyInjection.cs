f412d6ab6318a3b2ab735267d16b6856784404e8
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-17 18:30:05 +0500

    Dependencyinjection

--- CQRS_Service.Infrastructure/DependencyInjection.cs
+++ CQRS_Service.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,24 @@
-﻿namespace CQRS_Service.Infrastructure
+﻿using CQRS_Service.Application.Absreactions;
+using CQRS_Service.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CQRS_Service.Infrastructure
 {
-    public class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ICQRSDBContext, CQRSDBContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("Defoult"));
+            });
+
+            return services;
+        }
+
+
 
     }
 }
