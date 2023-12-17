4f7c820f75412967af420714976c8fdb4d61077e
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-17 18:28:04 +0500

    Create

--- CQRS_Service.Application/UseCases/Users/Commands/CreateUserCommand.cs
+++ CQRS_Service.Application/UseCases/Users/Commands/CreateUserCommand.cs
@@ -2,7 +2,7 @@
 
 namespace CQRS_Service.Application.UseCases.User.Commands
 {
-    public class CreateUserCommand : IRequest
+    public class CreateUserCommand : IRequest<bool>
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
